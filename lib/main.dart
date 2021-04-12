86d0d20c298759e8eb787a5ae0a9ffd69731edde
Sidi Zawi <sidi@Sidis-MacBook-Air.local>
2021-04-13 00:10:02 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftNum = 1;
+  int rightNum = 1;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  leftNum = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset("images/dice$leftNum.png"),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  rightNum = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset("images/dice$rightNum.png")
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
